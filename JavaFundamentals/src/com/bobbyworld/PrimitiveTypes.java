53ed2eaa70bccaf5f96d5bbc390bed8cdcc40680
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-07-07 13:43:37 -0500

    primitivie types

---
+++ JavaFundamentals/src/com/bobbyworld/PrimitiveTypes.java
@@ -0,0 +1,14 @@
+package com.bobbyworld;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+//        ALL TYPE BELOW ARE PRIMITIVE TYPES
+        byte age = 30;
+        int viewsCount = 123_456_789;
+        long viewsCounter = 3_123_456_789L; // added the L to the end due to compiler looks at the value as an integer
+        double price = 10.99;
+        float otherPrice = 11.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+    }
+}
