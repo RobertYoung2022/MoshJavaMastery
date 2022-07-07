647098a2e2da90c480c99822b09426a143a42c3b
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-07-07 00:08:13 -0500

    Starting Java Fundamentals with Mosh

---
+++ JavaFundamentals/src/com/bobbyworld/Main.java
@@ -0,0 +1,11 @@
+package com.bobbyworld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+// Compilation -> Execution
+
+// Source Code  >  Java Compiler  >  Byte Code
+// *.java                            *.class
